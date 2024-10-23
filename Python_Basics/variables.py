71fa0c97903a5050528d7cef16b72c7b39236edb
priscilla-akorfa <fomevorpriscillaakorfa6@gmail.com>
2024-10-23 13:56:52 +0000

    question 2

---
+++ Python_Basics/variables.py
@@ -0,0 +1,8 @@
+#!//usr/bin/python3
+
+name = 'Hello, my name is Akorfa.'
+age = 'I am 23 years old'
+city = 'and I live in Accra.'
+
+entire_message = f'{name} {age} {city}'
+print(entire_message) 
