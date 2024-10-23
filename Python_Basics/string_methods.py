b0969dc881d28846e76fdeeb62206e44139226ea
priscilla-akorfa <fomevorpriscillaakorfa6@gmail.com>
2024-10-23 14:29:25 +0000

    question 3

---
+++ Python_Basics/string_methods.py
@@ -0,0 +1,10 @@
+#!//usr/bin/python3
+
+#message = 'Hello, my name is Priscilla'
+#print(message.upper())
+#print(message.lower())
+#print(message.replace('Priscilla', 'Akorfa')) 
+
+name = 'Hello Akorfa,'
+greeting = 'Good day.'
+print(name + ' ' + greeting)
