b0de2a6b3927561e0d5f935dae7e766aca142ae1
priscilla-akorfa <fomevorpriscillaakorfa6@gmail.com>
2024-10-28 16:33:40 +0000

    question 2

--- Python_Control_Flow/list_and_set_methods.py
+++ Python_Control_Flow/list_and_set_methods.py
@@ -1,2 +1,45 @@
 #!/usr/bin/python3
-list1 = [Akorfa, Jenny,
+names = ['Akorfa', 'Jenny', 'Sam', 'Dora'] 
+print(names)
+fruits = ['Apple', 'Kiwi', 'Mango']
+print(fruits)
+
+names.append('Dee')
+print(names)
+
+names.pop()
+print(names)
+
+names.remove('Sam')
+print(names)
+
+names.sort()
+print(names)
+
+names.reverse
+print(names)
+
+names.extend(fruits)
+print(names)
+
+
+# ii
+Odd = {1, 3, 5, 7}
+Even = {2, 4, 6, 8}
+print(Odd)
+
+Odd.add(2)
+print(Odd)
+
+Odd.remove(1)
+print(Odd)
+
+Odd.union(Even)
+print(Odd)
+
+Odd.intersection(Even)
+print(Odd)
+
+Odd.difference(Even)
+print(Odd)
+
