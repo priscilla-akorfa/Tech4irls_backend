8dc274fc13d325e5aebcb85001f9aea7059e7fcd
priscilla-akorfa <fomevorpriscillaakorfa6@gmail.com>
2024-11-01 15:23:35 +0000

    question4

---
+++ Python_Control_Flow/control_flow.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+age = int(input())
+if age < 18:
+   print('You are a minor')
+elif age >= 18 & age >= 64:
+   print('You are an adult')
+else:
+   print('You are a senior')       
+
