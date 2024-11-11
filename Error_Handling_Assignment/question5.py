85ff65928410bbdd89b6852a8b80984af56a92a9
priscilla-akorfa <fomevorpriscillaakorfa6@gmail.com>
2024-11-11 16:22:45 +0000

    question 5

---
+++ Error_Handling_Assignment/question5.py
@@ -0,0 +1,12 @@
+#Question 6: Age Validator
+
+try:
+    age = int(input("Enter your age: "))
+    if age < 0:
+        print("Age cannot be negative!")
+    elif age > 120:
+        print("That age is unlikely!")
+    else:
+        print("Your age is:", age)
+except ValueError:
+    print("Heyya, enter a valid integer for your age.")
